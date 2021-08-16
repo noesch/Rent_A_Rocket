87907fba8f2df37c1963c8efa4f2fbab0cd647dc
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-16 17:38:56 +0200

    added the rocket model and migrated its table to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_150323) do
+ActiveRecord::Schema.define(version: 2021_08_16_153326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rockets", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "fuel"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
