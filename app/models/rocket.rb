591001997f2f673e06ac4cb837c48fec09afc071
Janus Vollmer <janus.vollmer@gmail.com>
2021-08-17 15:20:40 +0200

    Added the seed file and some styling for rockets index page and rocket show page

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -1,2 +1,3 @@
 class Rocket < ApplicationRecord
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing database..."
+
+Rocket.destroy_all
+User.destroy_all
+
+user = User.create(email: "test@test.com", password: "123123")
+
+puts "Building database..."
+
+rocket_one = { name: "Falcon 9", price: 10000, fuel: "Kerosin", description: "really cool spaceship", user: user }
+rocket_two = { name: "Blue Origin", price: 20000, fuel: "H2O", description: "Amazong rocket", user: user }
+rocket_three = { name: "Challenger", price: 5, fuel: "Diesel", description: "For the daring", user: user }
+rocket_four = { name: "Saturn 5", price: 8000, fuel: "Corn starch", description: "Not fancy but it'll do", user: user }
+rocket_five = { name: "UFO", price: 500000000, fuel: "Micro Black Holes", description: "Welcome to the future", user: user }
+
+[rocket_one, rocket_two, rocket_three, rocket_four, rocket_five].each do |rocket|
+  Rocket.create(rocket)
+end
+
+puts "Finished"
