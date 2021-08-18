e4d827266913ac3be862fcf02ccc7efd4c6e5d5f
Max Eduard von Wedemeyer <max-eduard@t-online.de>
2021-08-18 16:14:05 +0200

    pundit setup and navbar

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -2,17 +2,22 @@ class RocketsController < ApplicationController
   before_action :set_rockets, only: [:index]
 
   def index
+    @rocket = policy_scope(Rocket).order(created_at: :desc)
   end
 
   def show
     @rocket = Rocket.find(params[:id])
+    @booking = @rocket.last_booking
+    authorize @rocket
   end
 
   def new
     @rocket = Rocket.new
+    authorize @rocket
   end
 
   def create
+    authorize @rocket
     @rocket = Rocket.new(rocket_params)
     if @rocket.save
       redirect_to rocket_path(@rocket)
